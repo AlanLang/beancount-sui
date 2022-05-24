82737cbdf017146789b908325359818f571e1b80
alan <alan@fanruan.com>
2022-05-24 11:39:55 +0800

    first commit

---
+++ src/user.ts
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Userscript
+// @version      0.0.1
+// @author       my
+// @include      https://nhentai.net/*
+// @noframes
+// @description  my userscript
+// ==/UserScript==
