6678c149588963b61b4454d86bcd77c1e32e3573
alan <langwdalan@gmail.com>
2022-09-03 15:20:32 +0800

    update

--- src/user.ts
+++ src/user.ts
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         随手机账单
+// @name         随手记账单
 // @version      0.0.1
 // @author       my
 // @include      https://www.sui.com/*
 // @noframes
-// @description  随手机转Bean
+// @description  随手记转Bean
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
